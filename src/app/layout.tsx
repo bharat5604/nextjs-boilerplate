42f3af59ec03eb69bde7916c3aef5f718cc8c7e0
bharat <bhushansingh696@gmail.com>
2023-07-23 21:57:45 +0530

    adding header

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import ProviderRedux from "@/redux/provider";
 import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import Header from "@/components/Header";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,7 +19,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <ProviderRedux>{children}</ProviderRedux>
+        <ProviderRedux>
+          <Header />
+          {children}
+        </ProviderRedux>
       </body>
     </html>
   );
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,18 @@
+import Link from "next/link";
 import React from "react";
 
 type Props = {};
 
 const Header = (props: Props) => {
-  return <div>Header</div>;
+  return (
+    <div className="bg-blue-700 py-2">
+      <div className="container">
+        <div className="flex">
+          <Link href="/">Home</Link>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Header;
