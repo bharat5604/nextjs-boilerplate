e9dd30f756842ea439195af7f59173de58961b2f
bharat <bhushansingh696@gmail.com>
2023-07-24 21:57:05 +0530

    adding auth api

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,14 +1,34 @@
+"use client";
+
 import Link from "next/link";
 import React from "react";
 
 type Props = {};
 
 const Header = (props: Props) => {
+  const user = {
+    username: "bharat",
+    password: "test",
+  };
+  const handleLogin = async () => {
+    try {
+      await fetch(`http://localhost:8000/auth`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(user),
+      }).then((res) => res.json());
+    } catch (error) {
+      console.log("eror", error);
+    }
+  };
   return (
     <div className="bg-blue-700 py-2">
       <div className="container">
         <div className="flex">
           <Link href="/">Home</Link>
+          <button onClick={handleLogin}>Login</button>
         </div>
       </div>
     </div>
