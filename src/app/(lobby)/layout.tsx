b28149b621d6b30b6fe420128cd4fff8f69c0abe
bharat <bhushansingh696@gmail.com>
2023-08-13 17:57:40 +0530

    adding lobby layout

---
+++ src/app/(lobby)/layout.tsx
@@ -0,0 +1,26 @@
+// import { currentUser } from "@clerk/nextjs";
+
+import { SiteFooter } from "@/components/layouts/site-footer";
+import { SiteHeader } from "@/components/layouts/site-header";
+
+interface LobbyLayoutProps {
+  children: React.ReactNode;
+}
+
+export default async function LobbyLayout({ children }: LobbyLayoutProps) {
+  //   const user = await currentUser();
+  const user = {
+    firstName: "bharat",
+    lastName: "singh",
+    imageUrl: "",
+    username: "bharat",
+  };
+
+  return (
+    <div className="relative flex min-h-screen flex-col">
+      <SiteHeader user={user} />
+      <main className="flex-1">{children}</main>
+      <SiteFooter />
+    </div>
+  );
+}
