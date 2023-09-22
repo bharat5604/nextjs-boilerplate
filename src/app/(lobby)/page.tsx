18fbbad1e900109950a7bd308c1172626fcd9298
bharat <bhushansingh696@gmail.com>
2023-09-23 01:37:32 +0530

    adding redux

--- src/app/(lobby)/page.tsx
+++ src/app/(lobby)/page.tsx
@@ -1,7 +1,19 @@
 import { Shell } from "@/components/shells/shell";
+import { logIn } from "@/redux/features/authSlice";
+import { store } from "@/redux/store";
 import React from "react";
 
-const Lobby = () => {
+const testRedux = async () => {
+  try {
+    return store.dispatch(logIn("bharat"));
+  } catch (error) {
+    console.log("error", error);
+  }
+};
+
+const Lobby = async () => {
+  const data = await testRedux();
+  console.log("hello", data);
   return <Shell>page</Shell>;
 };
 
